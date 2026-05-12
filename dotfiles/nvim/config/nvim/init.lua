6bb3e3d4586615b85d7032acea1c6ccca8df4ee2
DJ Ambrisco <dambrisco@gmail.com>
2026-05-12 16:22:25 -0600

    feat: show hidden files by default in nvim

--- dotfiles/nvim/config/nvim/init.lua
+++ dotfiles/nvim/config/nvim/init.lua
@@ -42,7 +42,7 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+      { "<leader>ff", function() require("telescope.builtin").find_files({ hidden = true }) end, desc = "Find files" },
       { "<leader>fg", function() require("telescope.builtin").live_grep() end, desc = "Live grep" },
       { "<leader>fb", function() require("telescope.builtin").buffers() end, desc = "Buffers" },
       { "<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Help tags" },
@@ -88,6 +88,11 @@ require("lazy").setup({
       filesystem = {
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
       },
     },
   },
